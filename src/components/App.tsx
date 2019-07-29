b1664d59d3b49dae2fa7e8b1e35d28239e059d2e
Marcin <jerzakm@gmail.com>
2019-07-29 19:41:22 +0200

    card and list skeleton

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import TrelloList from "./TrelloList";
 
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>testing...</h1>
+        <TrelloList title="test"/>
       </div>
     )
   }
